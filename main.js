5bfbc9151c314306e28863566f3e0a4c95906ef4
xjhxcc <2351889342@qq.com>
2019-07-11 00:24:18 +0800

    refactor:pos main,add functions

--- main.js
+++ main.js
@@ -1,31 +1,71 @@
-function printReceipts (arr,collection) {
-    var name,count,price,sum;var brr=[];
-    var result_child=[];
-    var result=[];
-    var str=["Receipts","------------------------------------------------------------"];
-    for(var j=0;j<collection.length;j++){
-        for(var i=0;i<arr.length;i++){
-            if(collection[j].id==arr[i]){
-                name=collection[j].name;
-                if(brr.length<=0){
-                    brr.push({
-                        foodname:name,
-                        foodcount:count
-                    });
-                }                
-                price=collection[j].price;
-                count++;
-                sum+=price;
-                brr=[];
-                result_child.push(name+"\t"+price+"\t"+count);
-                result.push(result_child);
-            }else{
-                return "error";
+const isBarcodesValid= (baseData,inputFoodItem) =>{
+    let result;
+    for(let i=0;i<baseData.length;i++){
+        for(let j=0;j<inputFoodItem.length;j++){
+            if(baseData[i].id!=inputFoodItem[j]){
+                result="error";
+                break;
             }
         }
     }
-    reutrn result.join('\n');
+    return result;
 }
 
 
-module.exports = printReceipts;
+const getFoodItemsByInputBarcodes=(baseData,inputFoodItem)=>{
+    if(!isBarcodesValid(baseData,inputFoodItem)){
+        return [false];
+    }  
+    let inputBarcodes=[];
+    for(let i=0;i<baseData.length;i++){
+        for(let j=0;j<inputFoodItem.length;j++){
+            if(baseData[i].id===inputFoodItem[j]){
+                inputBarcodes.push(baseData[i]);
+            }
+        }
+    }
+    return inputBarcodes;
+}
+
+const combineFoodItem=(baseData,inputFoodItem)=>{
+    let beCombinedBarcodes=getFoodItemsByInputBarcodes(baseData,inputFoodItem);
+    console.log(beCombinedBarcodes);
+     let allCombinedBarcodes=[];
+    for(let i=0;i<=beCombinedBarcodes.length;i++){
+        let beCombinedBarcodesArr=[];
+          beCombinedBarcodesArr.push(beCombinedBarcodes[i]);
+         beCombinedBarcodesArr["number"]=1;
+         // beCombinedBarcodes[i]["number"]=1;
+        for(let j=0;j<=beCombinedBarcodes.length;j++){
+            if(beCombinedBarcodes[i]==beCombinedBarcodes[j]){
+               // beCombinedBarcodes[i]["number"]++;
+            }
+        }
+          allCombinedBarcodes.push(beCombinedBarcodesArr);
+         beCombinedBarcodesArr=[];
+    }
+    return allCombinedBarcodes;
+}
+const printReceiptsByBarcodes = (baseData,inputFoodItem) =>{
+    if(!isBarcodesValid(baseData,inputFoodItem)){
+        return;
+    }
+    let receiptsList=combineFoodItem();
+    let summoney=0;
+    let str="Receipts\n------------------------------------------------------------\n";
+    for(let j=0;j<receiptsList.length;j++){
+        str+='${receiptsList[i].name}\t${receiptsList[i].price}\t${receiptsList[i].number}\n';
+        summoney+=receiptsList[i].price*receiptsList[i].number;
+        str+=`------------------------------------------------------------\nPrice:${summoney}`;
+    }
+    return str;
+}
+
+
+module.exports = {
+    isBarcodesValid,
+    getFoodItemsByInputBarcodes,
+    combineFoodItem,
+    printReceiptsByBarcodes
+};
+
