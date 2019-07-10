c8a007a7041b67aea8c4eb51a20438333d4abe67
xjhxcc <2351889342@qq.com>
2019-07-11 00:23:46 +0800

    refactor:pos-test

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -1,6 +1,9 @@
 const main = require('../main');
-const printReceipts = main.printReceipts;
-const collection=[
+const isBarcodesValid = main.isBarcodesValid;
+const getFoodItemsByInputBarcodes= main.getFoodItemsByInputBarcodes;
+const combineFoodItem=main.combineFoodItem;
+const printReceiptsByBarcodes=main.printReceiptsByBarcodes;
+const baseData=[
     {"id": "0001", "name" : "Coca Cola", "price": 3},
     {"id": "0002", "name" : "Diet Coke", "price": 4},
     {"id": "0003", "name" : "Pepsi-Cola", "price": 5},
@@ -12,16 +15,27 @@ const collection=[
     {"id": "0009", "name" : "Diet Dr Pepper", "price": 11},
     {"id": "0010", "name" : "Fanta", "price": 12}
     ]
-it ('should return true when call print receipts(['0001', '0003', '0005', '0003'])', () => {
-    expect(printReceipts(['0001', '0003', '0005', '0003'],collection)).toBe(
-        "Receipts\n"
-        +"------------------------------------------------------------\n"
-        +"Coca Cola                       3          1\n"
-        +"Pepsi-Cola                      5          2\n"   
-        +"Dr Pepper                       7          1\n"
-        +"------------------------------------------------------------\n"
-        +"Price: 20"
 
+it ('should return true when call isBarcodesValid', () => {
+    expect(isBarcodesValid(baseData,['0012', '0003', '0005', '0003'])).toBe("error");
+});
+
+it ('should return true when call getFoodItemsByInputBarcodes', () => {
+    expect(getFoodItemsByInputBarcodes(baseData,['0001', '0003', '0003', '0005'])).toEqual(
+        [{"id": "0001", "name" : "Coca Cola", "price": 3},
+        {"id": "0003", "name" : "Pepsi-Cola", "price": 5},
+        {"id": "0003", "name" : "Pepsi-Cola", "price": 5},
+        {"id": "0005", "name" : "Dr Pepper", "price": 7}]
     );
 });
+
+it ('should return true when call combineFoodItem', () => {
+    expect(combineFoodItem(baseData,['0001', '0003', '0003', '0005'])).toEqual(
+        [{"id": "0001", "name" : "Coca Cola", "price": 3,"number":1},
+        {"id": "0003", "name" : "Pepsi-Cola", "price": 5,"number":2},
+        {"id": "0003", "name" : "Pepsi-Cola", "price": 5,"number":2},
+        {"id": "0005", "name" : "Dr Pepper", "price": 7,"number":1}]
+    );
+});
+
 
